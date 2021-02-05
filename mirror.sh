6f6c87fefee8f2c8e9e7ff84ecc684b84619e548
Eddie <11610634+twje@users.noreply.github.com>
2021-02-05 11:46:47 +1100

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -5,4 +5,4 @@ set -eu
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add target "$INPUT_TARGET_REPO_URL"
-git push target --mirror
+git push target --mirror "refs/remotes/origin/*:refs/heads/*"
